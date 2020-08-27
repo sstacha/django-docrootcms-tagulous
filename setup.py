94ae0a614c56f73149c00cd5dc9fb2789a69aca0
Steve Stacha <sstacha@spe.org>
2020-08-27 16:35:53 -0400

    adding python 3 patched version of django-tagulous with additional patch for pk

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='django-docrootcms-tagulous',
+      version='0.3',
+      description='Fork of the django-tagulous patched to work with Django 3',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/sstacha/django-docrootcms-tagulous',
+      author='Steve Stacha',
+      author_email='sstacha@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 3.0",
+      ],
+      python_requires='>=3.6',
+      install_requires=[
+          'django',
+      ],
+)
